d9c6ed7dec2ccf07dfc8ff93c508cda0e888d636
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2022-07-18 22:30:23 +0300

    Formatting

--- fsharp/Tests/Utils.fs
+++ fsharp/Tests/Utils.fs
@@ -9,16 +9,19 @@ let assertJsonEqual expected actual =
     let actualToken = JToken.Parse actual
     actualToken.Should().BeEquivalentTo(expected = expectedToken, because = "") // there is a default value "", bug in F# 
     |> ignore
-let user1={ Id=UserId 1
-            Login="user"; Password="psw"
-            Data = { Email="email@email.se"
-                     IsActive=true
-                     FirstName="Firstname"; LastName="Lastname"
-                     Roles=[Normal] } }
-let user2={ Id=UserId 2
-            Login="support";Password="psw"
-            Data = { Email="email2@email.se"
-                     IsActive=true
-                     FirstName="Firstname";LastName="Lastname"
-                     Roles=[Support] } }
+
+let user1 = { Id=UserId 1
+              Login="user"; Password="psw"
+              Data = { Email="email@email.se"
+                       IsActive=true
+                       FirstName="Firstname"; LastName="Lastname"
+                       Roles=[Normal] } }
+
+let user2 = { Id=UserId 2
+              Login="support";Password="psw"
+              Data = { Email="email2@email.se"
+                       IsActive=true
+                       FirstName="Firstname";LastName="Lastname"
+                       Roles=[Support] } }
+
 let users= [user1;user2]
