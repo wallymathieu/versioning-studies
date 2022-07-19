c9afa9df5a3cbef8448b8eb78a64747db8528e6d
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2022-07-19 11:03:45 +0300

    +mixcodec

--- fsharp/Tests/Tests.fs
+++ fsharp/Tests/Tests.fs
@@ -1,22 +1,23 @@
 module Wallymathieu.VersioningStudies.Tests
 open Xunit
+open Fleece
+open Fleece.FSharpData
 
 open Wallymathieu.VersioningStudies.Utils
 open Wallymathieu.VersioningStudies.JSON
-
-[<Fact>]
-let ``v1 format``() =
-    let expected = """{
+let v1UserJson = """{
   "id": 1,
   "email": "email@email.se",
   "name": "Firstname Lastname",
   "isActive": true,
   "roles": ["N"]}
 """
-    assertJsonEqual expected (string (V1.User.toJson user1))
+
 [<Fact>]
-let ``v2 format``() =
-    let expected = """{
+let ``v1 format``() =
+    assertJsonEqual v1UserJson (string (V1.User.toJson user1))
+
+let v2UserJson = """{
  "userUri": "/user/1",
  "email": "email@email.se",
  "name": {
@@ -26,4 +27,36 @@ let ``v2 format``() =
   "isActive": true,
   "roles": ["USR"]}
 """
-    assertJsonEqual expected (string (V2.User.toJson user1))
+
+[<Fact>]
+let ``v2 format``() =
+    assertJsonEqual v2UserJson (string (V2.User.toJson user1))
+
+let mixCodec = V2.UserData.codec <|> V1.UserData.codec |> ofObjCodec
+
+[<Fact>]
+let ``mix format v1 input``() =
+    let decoded = Codec.decode mixCodec (Encoding.Parse v1UserJson)
+    
+    match decoded with 
+    | Ok v->
+      Assert.Equal ("email@email.se", v.Email)
+      Assert.Equal ("Firstname", v.FirstName)
+      Assert.Equal ("Lastname", v.LastName)
+      Assert.Equal (true, v.IsActive)
+
+    | Error e-> failwithf "failed to decode %O" e
+
+[<Fact>]
+let ``mix format v2 input``() =
+    let decoded = Codec.decode mixCodec (Encoding.Parse v2UserJson)
+    
+    match decoded with 
+    | Ok v->
+      Assert.Equal ("email@email.se", v.Email)
+      Assert.Equal ("Firstname", v.FirstName)
+      Assert.Equal ("Lastname", v.LastName)
+      Assert.Equal (true, v.IsActive)
+
+    | Error e-> failwithf "failed to decode %O" e
+
