1c78242539612640d884dc7b0f2e112c2880865f
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2022-07-21 07:55:14 +0300

    +Lombok

--- java/src/main/java/se/gewalli/users/model/V2UserName.java
+++ java/src/main/java/se/gewalli/users/model/V2UserName.java
@@ -1,17 +1,19 @@
 package se.gewalli.users.model;
 
-import java.util.Objects;
-
 import org.springframework.validation.annotation.Validated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
 /**
  * V2UserName
  */
 @Validated
+@Accessors(chain = true,fluent = true)
+@Data
 public class V2UserName   {
   @JsonProperty("firstName")
   private String firstName = null;
@@ -19,10 +21,6 @@ public class V2UserName   {
   @JsonProperty("lastName")
   private String lastName = null;
 
-  public V2UserName firstName(String firstName) {
-    this.firstName = firstName;
-    return this;
-  }
 
   /**
    * Get firstName
@@ -32,15 +30,6 @@ public class V2UserName   {
   
     public String getFirstName() {
     return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public V2UserName lastName(String lastName) {
-    this.lastName = lastName;
-    return this;
   }
 
   /**
@@ -53,48 +42,5 @@ public class V2UserName   {
     return lastName;
   }
 
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
 
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    V2UserName v2UserName = (V2UserName) o;
-    return Objects.equals(this.firstName, v2UserName.firstName) &&
-        Objects.equals(this.lastName, v2UserName.lastName);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(firstName, lastName);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class V2UserName {\n");
-    
-    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
-    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
