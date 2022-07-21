f7995f88cc929b8531f4d76536f971e8a79124e8
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2022-07-21 07:56:38 +0300

    testing

--- clojure/src/app/core.clj
+++ clojure/src/app/core.clj
@@ -1,6 +1,19 @@
 (ns app.core)
+(require '[reitit.ring :as ring])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler [_]
+  {:status 200, :body "ok"})
+
+(defn wrap [handler id]
+  (fn [request]
+    (update (handler request) :wrap (fnil conj '()) id)))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      ["/api" {:middleware [[wrap :api]]}
+       ["/ping" {:get handler
+                 :name ::ping}]
+       ["/admin" {:middleware [[wrap :admin]]}
+        ["/users" {:get handler
+                   :post handler}]]])))
